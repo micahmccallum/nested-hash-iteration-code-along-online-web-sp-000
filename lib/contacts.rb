339ae368a21006deaab04906cdcafb2342731580
Micah McCallum <micahmccallum@outlook.com>
2020-03-29 03:17:14 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |name, data|
     data.each do |attribute, details|
       if attribute == :favorite_ice_cream_flavors
-        details.delete("strawberry")
+        details.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
